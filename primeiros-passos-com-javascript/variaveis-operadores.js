6a8b93170cbd18cfb045a4889a497b2625d578ae
Andrelon de Souza <dedeh.souza2019@gmail.com>
2025-04-28 14:27:41 -0300

    Estudando Variáveis e Operadores em Javascript e fazendo exercício de fixação

---
+++ primeiros-passos-com-javascript/variaveis-operadores.js
@@ -0,0 +1,26 @@
+ variavel = 40;
+console.log(variavel);
+
+
+variavel = 10;
+const pi = 3.14;
+
+
+console.log(variavel);
+
+// pi = 3.13 Não é possível
+
+let soma = 30 + 29;
+
+console.log(soma);
+
+
+// Exercício de fixação:
+
+let precoCombustivel = 5.69;
+let gastoMedioPorKm = 9;
+let distanciaViagem = 140;
+
+let gastoViagem = (distanciaViagem / gastoMedioPorKm) * precoCombustivel;
+
+console.log(`O gasto para realizar a sua viagem será de R$${gastoViagem.toFixed(2)}`);
